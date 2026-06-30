f434b3f04abbb4df03909eb0ebdc4088dbea8125
ivrishhhh <alla.rishivenkatesh@gmail.com>
2026-06-30 19:29:06 +0530

    backend deployed

--- frontend/src/utils/ApiRequest.js
+++ frontend/src/utils/ApiRequest.js
@@ -1,6 +1,4 @@
-// const host = "https://expense-tracker-app-knl1.onrender.com";
-// const host = "https://wisepay-personal-expense-tracker-for.onrender.com";
-const host = "http://localhost:5000";
+const host = "https://wisepay.onrender.com";
 export const setAvatarAPI = `${host}/api/auth/setAvatar`;
 export const changeNameAPI = `${host}/api/auth/changeName`;
 export const changePasswordAPI = `${host}/api/auth/changePassword`;
