13e09be1a2cc39c65eed5115b3f2bddd7ed3048e
ivrishhhh <alla.rishivenkatesh@gmail.com>
2026-07-02 19:25:39 +0530

    Final Changes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,6 +5,7 @@ import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { changeNameAPI, changePasswordAPI } from "../utils/ApiRequest";
+import { Button } from '@mui/material';
 
 const Header = () => {
   const navigate = useNavigate();
